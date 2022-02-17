49adf084e666817d40b958c5f60a343a85a54829
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-02-17 23:20:33 +0800

    Tugas 19 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+function tugas19(){
+    var kalimat = 'semangat...';
+    var bintang= '*****';
+    
+    console.log(kalimat.repeat(5));
+    console.log("");
+    console.log(bintang);
+    console.log(bintang.repeat(5));
+    console.log(bintang.repeat(10));
+    console.log(bintang.repeat(13));
+    console.log(bintang.repeat(18));
+    console.log(bintang.repeat(13));
+    console.log(bintang.repeat(10));
+    console.log(bintang.repeat(5));
+    console.log(bintang);
+
+    
+    
+}
+tugas19();
